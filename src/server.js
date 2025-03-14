584023d4967c95f9613f7350a8aebd774dbfefd1
thiagodevmaster <thiagodantas.dev@gmail.com>
2025-03-13 23:20:36 -0300

    teste

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 import app from "./app.js";
 import { prisma, testConnection } from "./config/db.js";
 
-async function startServer(PORT) {
-    await testConnection();
+const PORT = process.env.PORT || 3000;
 
-    app.listen(PORT, () => {
-        console.log(`🚀 Servidor rodando na porta ${PORT}`);
-    });
+async function startServer(PORT) {
+    try {
+        await testConnection();
+
+        app.listen(PORT, () => {
+            console.log(`🚀 Servidor rodando na porta ${PORT}`);
+        });
+    } catch (error) {
+        console.error("❌ Erro ao iniciar o servidor:", error);
+        process.exit(1);
+    }
 }
 
-startServer(3000).catch((error) => {
-    console.error("❌ Erro ao iniciar o servidor:", error);
-    process.exit(1);
-});
+startServer(PORT);
