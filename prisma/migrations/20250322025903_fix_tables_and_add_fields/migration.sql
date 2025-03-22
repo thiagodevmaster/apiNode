6aeff59ada0aae0a0a796317ff462e1722a74226
thiagodevmaster <thiagodantas.dev@gmail.com>
2025-03-21 23:59:53 -0300

    teste

---
+++ prisma/migrations/20250322025903_fix_tables_and_add_fields/migration.sql
@@ -0,0 +1,77 @@
+/*
+  Warnings:
+
+  - You are about to drop the `category` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `task` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `task` DROP FOREIGN KEY `Task_category_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `task` DROP FOREIGN KEY `Task_user_id_fkey`;
+
+-- DropTable
+DROP TABLE `category`;
+
+-- DropTable
+DROP TABLE `task`;
+
+-- DropTable
+DROP TABLE `user`;
+
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `profileImage` VARCHAR(255) NULL,
+    `lastAccess` DATETIME(3) NULL,
+    `preferences` JSON NULL,
+    `bio` TEXT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `tasks` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    `description` TEXT NOT NULL,
+    `dueDate` DATETIME(3) NOT NULL,
+    `priority` ENUM('alta', 'media', 'baixa') NOT NULL DEFAULT 'media',
+    `status` ENUM('pendente', 'completa') NOT NULL DEFAULT 'pendente',
+    `reminder` DATETIME(3) NOT NULL,
+    `tags` TEXT NOT NULL,
+    `estimatedTime` INTEGER NOT NULL,
+    `recurrence` VARCHAR(191) NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `category_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `categories` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `user_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `tasks` ADD CONSTRAINT `tasks_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `tasks` ADD CONSTRAINT `tasks_category_id_fkey` FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `categories` ADD CONSTRAINT `categories_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
